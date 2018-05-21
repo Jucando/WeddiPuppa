3b98500ba7db8254c6c826be75a3fc990c61feef
DelphineHélie <37551188+DelphineHelie@users.noreply.github.com>
2018-05-21 17:53:01 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,3 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index, :show, :book]
-
-  def home
-  end
-
-  def index
-  end
-
-  def show
-  end
-
-  def book
-  end
+  skip_before_action :authenticate_user!, only: [:home]
 end
