20a3ebdac4df60e0e95652b09065f55632a448ae
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-21 18:08:21 +0200

    validates : change name by first_name and delete validates on email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true
+  validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, uniqueness: true, presence: true
   validates :address, presence: true
 end
