1e78b1d8a182ba46b90b82fdafebaa0c7b72b4e4
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-21 16:39:41 +0200

    validates 3 models

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,3 +1,7 @@
 class Performance < ApplicationRecord
   belongs_to :user
+  validates :category, inclusion: { in: ["Concert", "Danse", "Stand-up", "Painter", "DJ"] }
+  validates :price, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :performance
   belongs_to :user
+  validates :date, presence: true
+  validates :location, presence: true
 end
