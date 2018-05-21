4b45f797deb71cfedd1c3ca6d5c546090b673f3d
Delphine Helie <heliedelphine@yahoo.fr>
2018-05-21 16:41:25 +0200

    PerformancesController setting up

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,6 +1,43 @@
 class PerformancesController < ApplicationController
   def index
-
+    @performances = performance.all
   end
 
+  def show
+    @performance = Performance.find(params[:id])
+  end
+
+  def new
+    @performance = Performance.new(performance_params)
+  end
+
+  def create
+    @performance = Performance.new(performance_params)
+    @performance.save
+  end
+
+  def edit
+    @performance = Performance.find(params[:id])
+  end
+
+  def update
+    @performance = Performance.find(params[:id])
+    @performance.update(performance_params)
+  end
+
+  def destroy
+    @performance = Performance.find(params[:id])
+    @performance.destroy
+    # no need for app/views/performances/destroy.html.erb
+    redirect_to performances_path
+  end
+
+  private
+
+  def performance_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:performance).permit(:title, :description, :price, :is_visible, :user)
+  end
 end
+
