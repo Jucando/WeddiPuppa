e6bc0ad577f4554c97b6a5f2e497c7c93974be46
Delphine Helie <heliedelphine@yahoo.fr>
2018-05-21 18:17:16 +0200

    fixed issue

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,11 +1,9 @@
 class Performance < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
-  has_many :media, :reservations
-=======
+  has_many :media
+  has_many :reservations
   validates :category, inclusion: { in: ["Concert", "Danse", "Stand-up", "Painter", "DJ"] }
   validates :price, presence: true
   validates :title, presence: true
   validates :description, presence: true
->>>>>>> 77e8e725702bd970c4960242747c20a968cbbd2f
 end
