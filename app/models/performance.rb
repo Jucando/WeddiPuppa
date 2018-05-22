5ee0bb22211fcce024a395bc7955a0d1f5eaebfd
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-22 10:59:49 +0200

    dependances media & reservations on performance

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,7 +1,7 @@
 class Performance < ApplicationRecord
   belongs_to :user
-  has_many :media
-  has_many :reservations
+  has_many :media, dependent: :nullify
+  has_many :reservations, dependent: :nullify
   validates :category, inclusion: { in: ["Concert", "Danse", "Stand-up", "Painter", "DJ"] }
   validates :price, presence: true
   validates :title, presence: true
