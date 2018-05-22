0b7141db3ef6fae5bca3490e816de5ed205ef71a
Julien Chim <julien.chim@gmail.com>
2018-05-22 14:26:55 +0200

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'rails/destroy'
-  get 'rails/Reservation'
-  get 'rails/destroy'
-  get 'rails/Performance'
   devise_for :users
   root to: 'pages#home'
   resources :performances do
