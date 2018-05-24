4f4bc9b5624cb252e211964368d025956ab11914
Delphine Helie <heliedelphine@yahoo.fr>
2018-05-24 10:26:04 +0200

    home updates

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'rails', '5.2.0'
 gem 'redis'
 gem 'pundit'
 gem 'geocoder'
+gem 'jquery-rails'
 
 
 gem 'autoprefixer-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,6 @@
 import "bootstrap";
 import "plugins/flatpickr";
+import "components/testimonials";
+
+import { testimonials } from '../components/testimonials';
+testimonials();
