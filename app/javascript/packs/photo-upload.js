971b3e94ba09cd2cc6b791448f3f7841a3d028ab
Julien Chim <julien.chim@gmail.com>
2018-05-24 18:24:51 +0200

    javascript save correction

--- app/javascript/packs/photo-upload.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function() {
-  $(document).on('change', ':file', function() {
-    document.querySelector(".box-select").innerHTML = "<i class='fas fa-check'></i>&nbsp Loaded";
-    document.querySelector(".box-select").classList.add('box-select-filled');
-  });
-});
