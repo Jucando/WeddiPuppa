6e1cf45a05225eb1052f06abd0f5c1ad791bdac2
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-05-25 00:58:51 +0200

    created the dashboard logic, barely drafted the design (just a simple formating), added dashboard in the navbar when logged in

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,10 @@
 class ReservationsController < ApplicationController
   def index
-    skip_policy_scope
-    @reservations_as_client = current_user.reservations
-    @reservations_as_performer = current_user.reservations_as_performer
+    if params[:action] == "reservations_as_client"
+      reservation_as_client
+    else
+      reservation_as_performer
+    end
   end
 
   def show
@@ -60,6 +62,16 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def reservation_as_client
+    skip_policy_scope
+    @reservations_as_client = current_user.reservations
+  end
+
+  def reservation_as_performer
+    skip_policy_scope
+    @reservations_as_performer = current_user.reservations_as_performer
   end
 
   private
