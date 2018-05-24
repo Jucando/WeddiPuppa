a713ceda537b1f7870a399e069c7ba2233d02888
Julien Chim <julien.chim@gmail.com>
2018-05-24 13:54:48 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_091554) do
+ActiveRecord::Schema.define(version: 2018_05_24_112421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_05_23_091554) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.boolean "is_accepted", default: false
     t.index ["performance_id"], name: "index_reservations_on_performance_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
