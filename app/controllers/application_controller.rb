df403f6c786353578bc034ee8bbe4274ccd9d30d
Julien Chim <julien.chim@gmail.com>
2018-05-25 11:29:00 +0200

    profile edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,14 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :description, :avatar])
+  end
+
   private
 
   def skip_pundit?
